31c8aa2a434c095934fa8bf0706eb05321159a7a
John <J.G.Lokerse@gmail.com>
2021-08-28 23:51:35 +0200

    Added an 'echo' for gnome extension packages

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 cp .bash_profile ~/
 cp .aliases ~/
+
+echo "
+Manually install:
+dash-to-dock@micxgx.gmail.com
+clipboard-indicator@tudmotu.com
+openweather-extension@jenslody.de
+BingWallpaper@ineffable-gmail.com
+
+"
